679688b0004e7978b88da3dbfc158d227b52dd0b
DavidR <miners@grassbustersofdayton.com>
2026-02-14 10:45:47 -0500

    Revert "Update freetype.mk"
    This reverts commit 033eb4956fc1966fda63e350239b82ac27c4839c.

--- depends/packages/freetype.mk
+++ depends/packages/freetype.mk
@@ -1,11 +1,12 @@
 package=freetype
-$(package)_version=2.6.3
-$(package)_download_path=https://download.savannah.gnu.org/releases/freetype
+$(package)_version=2.5.3
+$(package)_download_path=https://downloads.sourceforge.net/$(package)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=533a1ca5d6595793725bca7641d9461a0f00dd1732dded3e4281196f5dd21736
+$(package)_sha256_hash=c0848b29d52ef3ca27ad92e08351f023c5e24ce8cea7d8fe69fc96358e65f75e
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-zlib --without-png --disable-shared --enable-static --with-pic
+  $(package)_config_opts=--without-zlib --without-png --disable-static
+  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
